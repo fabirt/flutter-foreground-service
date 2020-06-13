d3ecfdbc12f0c718d42d1fdfa472a856e0ad7be5
Fabirt <fandig27@gmail.com>
2020-06-13 12:32:15 -0500

    Deleting files

--- plugins/foreground_service/example/android/app/src/main/kotlin/com/fabirt/foreground_service_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.fabirt.foreground_service_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- plugins/foreground_service/ios/Classes/ForegroundServicePlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface ForegroundServicePlugin : NSObject<FlutterPlugin>
-@end
